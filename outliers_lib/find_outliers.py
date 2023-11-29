451f8408bcea4c7e759ff3cf8ec2379582ff7828
Artemyi_Melehin <melehin2202@gmail.com>
2023-11-29 18:44:08 +0300

    add docs for quantiles_function

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -60,6 +60,17 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     return outliers, cleaned
 
 def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """Обработка выбросов данных, которая заключается в отбрасывании 2 % информации слева и справа в распределении.
+
+    Args:
+        data (_type_): _description_
+        feature (_type_): _description_
+        left (float, optional): _description_. Defaults to 0.01.
+        right (float, optional): _description_. Defaults to 0.99.
+
+    Returns:
+        _type_: _description_
+    """
     x = data[feature]
     lower_bound = x.quantile(left)
     upper_bound = x.quantile(right)
