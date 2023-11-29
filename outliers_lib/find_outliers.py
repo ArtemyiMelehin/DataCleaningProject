f0a721a26edc537da79aaddfd0e99d97e66757a9
Artemyi_Melehin <melehin2202@gmail.com>
2023-11-29 15:38:19 +0300

    Revert "add log func"
    This reverts commit d5ff24ab0fef4c593f46d0fb97689b366b0202b0.

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -59,6 +59,3 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
-
-def do_nothing():
-    pass
