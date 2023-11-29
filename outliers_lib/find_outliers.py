78112222a8479c11aad65272ead04ab4e24d221b
Artemyi_Melehin <melehin2202@gmail.com>
2023-11-29 18:28:17 +0300

    added function quantiles

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -59,3 +59,10 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    x = data[feature]
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
